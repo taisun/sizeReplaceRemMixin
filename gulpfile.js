a640cd89113c96aff9964adda13a70894dc57465
taisan <tairiho2008@gmail.com>
2015-05-23 03:34:27 +0900

    add

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+
+gulp.task('sass', function () {
+    var sassOption = {
+        style: 'expanded'
+    };
+    return $.rubySass('_src/sass/style.scss', sassOption)
+            .on('error', function (err) {
+                $.notify.onError('Error: <%= err.message %>');
+            })
+            .pipe($.plumber({
+                 errorHandler: $.notify.onError("Error: <%= error.message %>")
+            }))
+            .pipe(gulp.dest('_dest/css'));
+});
+
+gulp.task('watch', ['sass'], function () {
+    gulp.watch('_src/sass/**/*.scss', ['sass']);
+});
